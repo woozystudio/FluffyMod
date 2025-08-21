651bda54a953d2999c96516afb8bc3ab80ad3895
woozystudio <woozysstudio23@gmail.com>
2025-08-21 08:06:42 -0600

    feat: create guildinfo command

---
+++ apps/moderation/src/interactions/guildinfo.ts
@@ -0,0 +1,7 @@
+import { CommandPayload } from "@moderation/framework";
+
+export const guildinfoCommand: CommandPayload = {
+	name: "guildinfo",
+	description: "Get a detailed summary of this server.",
+	testMode: false,
+};
