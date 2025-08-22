f7247c69a5d35d3f0cb3d2bc62fb5b4013fb7824
woozystudio <woozysstudio23@gmail.com>
2025-08-21 18:18:23 -0600

    feat: create botinfo command

---
+++ apps/moderation/src/interactions/botinfo.ts
@@ -0,0 +1,7 @@
+import { CommandPayload } from "@moderation/framework";
+
+export const botinfoCommand: CommandPayload = {
+	name: "botinfo",
+	description: "Learn more about the bot.",
+	testMode: false,
+};
