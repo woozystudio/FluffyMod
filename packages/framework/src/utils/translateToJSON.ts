a386659541863785df2872746a6ed10f916f7d50
woozystudio <woozysstudio23@gmail.com>
2025-08-22 08:41:46 -0600

    chore: add .toString() to member perms in translateToJSON

--- packages/framework/src/utils/translateToJSON.ts
+++ packages/framework/src/utils/translateToJSON.ts
@@ -2,16 +2,14 @@ import { Collection } from "discord.js";
 import { Command } from "../structures/Command.js";
 import { CommandPayload } from "../types/Command.js";
 
-export function translateToJSON(
-	commands: Collection<string, Command<CommandPayload>>,
-): object[] {
+export function translateToJSON(commands: Collection<string, Command<CommandPayload>>): object[] {
 	const data: object[] = [];
 
 	commands.forEach((command: Command<CommandPayload>) => {
 		data.push({
 			name: command.name,
 			description: command.description,
-			default_member_permissions: command.default_member_permissions,
+			default_member_permissions: command.default_member_permissions?.toString(),
 			dm_permission: command.dm_permission,
 			options: command.options,
 		});
