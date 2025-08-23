ed106a0a81d1d14d638acf457c136ca2438600d0
woozystudio <woozysstudio23@gmail.com>
2025-08-23 16:15:59 -0600

    feat: create interactionCreate button execute event (#2)

--- apps/moderation/src/events/interactionCreate/interactionCreate.ts
+++ apps/moderation/src/events/interactionCreate/interactionCreate.ts
@@ -1,5 +1,5 @@
 import { ModerationClient, Event, EventPayload } from "@moderation/framework";
-import { ChatInputCommandInteraction, ClientEvents, Events } from "discord.js";
+import { ButtonInteraction, ChatInputCommandInteraction, ClientEvents, Events, MessageFlags } from "discord.js";
 
 export default class InteractionCreate extends Event<EventPayload, keyof ClientEvents> {
 	public constructor() {
@@ -9,20 +9,35 @@ export default class InteractionCreate extends Event<EventPayload, keyof ClientE
 		});
 	}
 
-	public async execute(client: ModerationClient, interaction: ChatInputCommandInteraction) {
-		if (!interaction.isChatInputCommand()) return;
+	public async execute(client: ModerationClient, interaction: ChatInputCommandInteraction | ButtonInteraction) {
+		if (interaction.isChatInputCommand()) {
+			const command = client.commands.get(interaction.commandName);
+			if (!command) return;
 
-		const command = client.commands.get(interaction.commandName);
-		if (!command) return;
+			try {
+				await command.chatInput(interaction, client);
+			} catch (error) {
+				console.error(`Error executing command ${interaction.commandName}:`, error);
+				await interaction.reply({
+					content: "There was an error while executing this command.",
+					flags: MessageFlags.Ephemeral,
+				});
+			}
+		}
 
-		try {
-			await command.chatInput(interaction, client);
-		} catch (error) {
-			console.error(`Error executing command ${interaction.commandName}:`, error);
-			await interaction.reply({
-				content: "There was an error while executing this command.",
-				ephemeral: true,
-			});
+		if (interaction.isButton()) {
+			const button = client.buttons.get(interaction.customId);
+			if (!button) return;
+
+			try {
+				await button.execute(interaction, client);
+			} catch (error) {
+				console.error(`Error executing button ${interaction.customId}:`, error);
+				await interaction.reply({
+					content: "There was an error while executing this button.",
+					flags: MessageFlags.Ephemeral,
+				});
+			}
 		}
 	}
 }
