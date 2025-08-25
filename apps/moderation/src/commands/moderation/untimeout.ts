61b28252c54641fa173b093d6f82d9729b72021a
woozystudio <woozysstudio23@gmail.com>
2025-08-25 15:02:09 -0600

    feat: create untimeout command

---
+++ apps/moderation/src/commands/moderation/untimeout.ts
@@ -0,0 +1,38 @@
+import { Command, CommandPayload } from "@moderation/framework";
+import {
+	ApplicationCommandOptionType,
+	ChatInputCommandInteraction,
+	GuildMember,
+	PermissionFlagsBits,
+} from "discord.js";
+import { Emoji } from "../../utils/constants.js";
+
+export default class UnTimeoutCommand extends Command<CommandPayload> {
+	public constructor() {
+		super({
+			name: "untimeout",
+			description: "Untimeout a server user.",
+			userPermissions: [PermissionFlagsBits.ModerateMembers],
+			options: [
+				{
+					name: "target",
+					description: "Select a server user.",
+					type: ApplicationCommandOptionType.User,
+					required: true,
+				},
+			],
+			testMode: true,
+		});
+	}
+
+	public async chatInput(interaction: ChatInputCommandInteraction) {
+		const target = interaction.options.getUser("target", true);
+		const member = (await interaction.guild?.members.fetch(target.id)) as GuildMember;
+
+		await member.timeout(null);
+
+		await interaction.reply({
+			content: `${Emoji.Success} The user ${target} has been untimeou successfully.`,
+		});
+	}
+}
