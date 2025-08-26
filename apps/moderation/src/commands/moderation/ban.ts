bce0528f2e04d871344983248a58979c40cba6c1
woozystudio <woozysstudio23@gmail.com>
2025-08-26 15:13:58 -0600

    feat: create ban command

---
+++ apps/moderation/src/commands/moderation/ban.ts
@@ -0,0 +1,38 @@
+import { Command, CommandPayload } from "@moderation/framework";
+import {
+	ApplicationCommandOptionType,
+	ChatInputCommandInteraction,
+	GuildMember,
+	PermissionFlagsBits,
+} from "discord.js";
+import { Emoji } from "../../utils/constants.js";
+
+export default class BanCommand extends Command<CommandPayload> {
+	public constructor() {
+		super({
+			name: "ban",
+			description: "Remove a member from the server with a ban.",
+			userPermissions: PermissionFlagsBits.BanMembers,
+			options: [
+				{
+					name: "target",
+					description: "Select a server user.",
+					type: ApplicationCommandOptionType.User,
+					required: true,
+				},
+			],
+			testMode: false,
+		});
+	}
+
+	public async chatInput(interaction: ChatInputCommandInteraction) {
+		const target = interaction.options.getUser("target", true);
+		const member = (await interaction.guild?.members.fetch(target.id)) as GuildMember;
+
+		await member.ban();
+
+		await interaction.reply({
+			content: `${Emoji.Success} User ${target} has been successfully banned from the server.`,
+		});
+	}
+}
