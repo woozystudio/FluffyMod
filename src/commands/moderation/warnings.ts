acea238e79d4f624a796d9eedf471521dbd34992
woozystudio <woozysstudio23@gmail.com>
2025-11-06 20:23:34 -0600

    feat: create warnings command

--- src/commands/moderation/warnings.ts
+++ src/commands/moderation/warnings.ts
@@ -1,5 +1,16 @@
 import { Command, CommandPayload } from "botten";
-import { ApplicationCommandOptionType, ChatInputCommandInteraction, PermissionFlagsBits } from "discord.js";
+import {
+	ApplicationCommandOptionType,
+	ChatInputCommandInteraction,
+	ContainerBuilder,
+	MessageFlags,
+	PermissionFlagsBits,
+	SectionBuilder,
+	SeparatorBuilder,
+	SeparatorSpacingSize,
+	TextDisplayBuilder,
+	ThumbnailBuilder,
+} from "discord.js";
 import Warning from "../../database/Warning.js";
 
 export default class WarningsCommand extends Command<CommandPayload> {
@@ -15,12 +26,6 @@ export default class WarningsCommand extends Command<CommandPayload> {
 					type: ApplicationCommandOptionType.User,
 					required: true,
 				},
-				{
-					name: "id",
-					description: "The ID of the warning.",
-					type: ApplicationCommandOptionType.Number,
-					required: false,
-				},
 			],
 			testMode: true,
 		});
@@ -28,17 +33,42 @@ export default class WarningsCommand extends Command<CommandPayload> {
 
 	override async chatInput(interaction: ChatInputCommandInteraction) {
 		const target = interaction.options.getUser("target", true);
-		const warningId = interaction.options.getNumber("id");
 
-		const warningID = await Warning.findOne({
+		// const fetchWarning = await Warning.findOne({
+		// 	GuildID: interaction.guild!.id,
+		// 	TargetID: target.id,
+		// });
+
+		const warningCount = await Warning.countDocuments({
 			GuildID: interaction.guild!.id,
 			TargetID: target.id,
-			where: { id: warningId },
 		});
 
-		console.log(warningID);
+		const warnings = await Warning.find({
+			GuildID: interaction.guild!.id,
+			TargetID: target.id,
+		});
 
-		// Missing code here for this system to be fully functional
-		await interaction.reply({ content: `This command is still being developed.`, ephemeral: true });
+		const header = new TextDisplayBuilder().setContent(`## Warnings for @${target.username}`);
+		const description = new TextDisplayBuilder().setContent(`Total warnings: **${warningCount}**`);
+		const thumbnail = new ThumbnailBuilder().setURL(target.displayAvatarURL({ size: 4096, extension: "png" }));
+		const separator = new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Large);
+
+		const section = new SectionBuilder().addTextDisplayComponents(header, description).setThumbnailAccessory(thumbnail);
+		const container = new ContainerBuilder().addSectionComponents(section).addSeparatorComponents(separator);
+
+		warnings.forEach((warning) => {
+			const warningField = new TextDisplayBuilder().setContent(
+				`**Moderator: ${warning.ModeratorUsername}**\n${warning.Reason} - <t:${warning.CreatedAt}:R>`,
+			);
+
+			container.addTextDisplayComponents(warningField);
+		});
+
+		await interaction.reply({
+			flags: [MessageFlags.IsComponentsV2],
+			components: [container],
+			allowedMentions: { parse: [] },
+		});
 	}
 }
