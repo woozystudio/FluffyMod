a5d9c830554e527a2bc9463bc88bcb6f99218021
woozystudio <woozysstudio23@gmail.com>
2025-11-08 16:31:09 -0600

    feat: create new color enum

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,10 @@ export enum Emoji {
 	Success = "<:checkmark:1408251573729103902>",
 	Error = "<:crossmark:1408251591550570506>",
 }
+
+export enum Color {
+	Danger = 0xc34e4e,
+	Success = 0x3fb97c,
+	Warning = 0xffcc4d,
+	Default = 0x2b2d31,
+}
